da151a5959994f5187923b422e78541610642267
DavneyRestraDanaz <davney.restra@mhs.itenas.ac.id>
2024-11-14 20:23:49 +0700

    5 detik splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void _navigateToMenu() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 5));
     Navigator.pushReplacementNamed(context, '/menu');
   }
 
